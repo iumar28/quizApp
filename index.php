29565ab6261b12523b0688937ef3f1ec2bb79769
iumar28 <umar9897222@gmail.com>
2023-01-18 16:21:32 +0530

    quizApp

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <!-- <head> -->
+  <meta charset="utf-8" />
+  <title>TinDog</title>
+
+  <!-- CSS Stylesheets -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+  <link rel="stylesheet" href="css/styles.css" />
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700;900&family=Ubuntu:wght@300&display=swap"
+    rel="stylesheet" />
+  <!-- Font Awesome -->
+  <script class="press-logo" src="https://kit.fontawesome.com/c75680ad7a.js" crossorigin="anonymous"></script>
+
+  <!-- Bootstrap Scripts -->
+  <script class="press-logo" src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
+  <script class="press-logo" src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script class="press-logo" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+    crossorigin="anonymous"></script>
+<!-- </head> -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>| Lorrain Music Academy |</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+        <link rel="shortcut icon" type="image/png" href="/image/lorrain.png" />
+        <style type="text/css">
+            body {
+                width: 100%;
+                background: url(image/book.png) ;
+                background-position: center center;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body>
+        <center>
+            <div class="intro">
+                <h1> Quiz System Umar Firoz </h1>
+                <a href="login.php" class="btn"> login </a> &emsp;
+                <a href="register.php" class="btn"> register </a>
+                <h2> Good &nbsp;Luck. </h2>
+            </div>
+        </center>
+    </body>
+</html>
